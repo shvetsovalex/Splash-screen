89115ae34f20182b94d742bd2b4aa113199c797e
Александр <shvetsovalex007@gmail.com>
2015-11-02 19:38:20 +0400

    gravity заменен на layout_gravity, использовано более подходящее имя класса(SplashScreen). Также для получения информации о том, создавалась ли активити используется массив типа Bandle

--- SplashScreen/app/src/main/java/com/example/splashscreen/SplashScreen.java
+++ SplashScreen/app/src/main/java/com/example/splashscreen/SplashScreen.java
@@ -5,30 +5,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class MainActivity extends Activity {
+public class SplashScreen extends Activity {
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 2000;
-    private static boolean splash = true;
+    private static final int SPLASH_TIME_OUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-        if(splash) {
+        if(savedInstanceState == null) {
             Handler handler = new Handler();
             Runnable runnable = new Runnable() {
                 @Override
                 public void run() {
-                    Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                    Intent i = new Intent(SplashScreen.this, HomeActivity.class);
                     startActivity(i);
                     finish();
                 }
             };
             handler.postDelayed(runnable, SPLASH_TIME_OUT);
         }
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        splash = false;
     }
 }
