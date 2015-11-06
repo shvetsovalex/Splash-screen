701a7430227183608ecdc9b1988f639f469745eb
Александр <shvetsovalex007@gmail.com>
2015-11-06 17:14:14 +0400

    Сделал через removeCallback.

--- SplashScreen/app/src/main/java/com/example/splashscreen/SplashScreen.java
+++ SplashScreen/app/src/main/java/com/example/splashscreen/SplashScreen.java
@@ -6,23 +6,27 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashScreen extends Activity {
-    // Splash screen timer
     private static final int SPLASH_TIME_OUT = 2000;
+    private Handler handler;
+    private Runnable runnable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-        if(savedInstanceState == null) {
-            Handler handler = new Handler();
-            Runnable runnable = new Runnable() {
-                @Override
-                public void run() {
-                    Intent i = new Intent(SplashScreen.this, HomeActivity.class);
-                    startActivity(i);
-                    finish();
-                }
-            };
-            handler.postDelayed(runnable, SPLASH_TIME_OUT);
-        }
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent in = new Intent(SplashScreen.this, HomeActivity.class);
+                startActivity(in);
+                finish();
+            }
+        };
+        handler.postDelayed(runnable, SPLASH_TIME_OUT);
+}
+    @Override
+    public void onSaveInstanceState(Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        handler.removeCallbacks(runnable);
     }
 }
